58025756360d810644d9edda0e1faa66f8999414
Gabriel Rodríguez <gabrielrodriguezj@hotmail.com>
2024-04-02 21:43:35 -0600

    Update StmtBlock.cpp
    Creación del environment local para cada block

--- src/Inter/Statement/StmtBlock.cpp
+++ src/Inter/Statement/StmtBlock.cpp
@@ -4,7 +4,10 @@
 StmtBlock::StmtBlock(const std::list<Statement *> &statements) : statements(statements) {}
 
 void StmtBlock::execute(Environment* environment) {
+    // Local environment for local variables
+    Environment* local = new Environment(environment);
+
     for(Statement *statement : statements){
-        statement->execute(environment);
+        statement->execute(local);
     }
 }
