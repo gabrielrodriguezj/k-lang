b5e833de55c0e36187f1897c8d6a4cd9beafaefe
Gabriel Rodríguez <gabrielrodriguezj@hotmail.com>
2024-04-02 21:27:58 -0600

    Update StmtIf.cpp

--- src/Inter/Statement/StmtIf.cpp
+++ src/Inter/Statement/StmtIf.cpp
@@ -1,10 +1,25 @@
 #include "StmtIf.h"
-#include "../../Exceptions/NotImplementedYetException.h"
+#include "../../Exceptions/RuntimeException.h"
 
 StmtIf::StmtIf(Expression *condition, Statement *thenBranch, Statement *elseBranch) : condition(condition),
                                                                                       thenBranch(thenBranch),
                                                                                       elseBranch(elseBranch) {}
 
 void StmtIf::execute(Environment* environment) {
-    throw NotImplementedYetException("Not implemented yet");
+    TData solCondition = condition->solve(environment);
+
+    if (!std::holds_alternative<bool>(solCondition.getValue())) {
+        throw RuntimeException("La condicion no es una expresion valida");
+    }
+
+    bool resCondition = std::get<bool>(solCondition.getValue());
+    if(resCondition){
+        thenBranch->execute(environment);
+        return;
+    }
+
+    if(elseBranch != nullptr){
+        elseBranch->execute(environment);
+    }
+    
 }
