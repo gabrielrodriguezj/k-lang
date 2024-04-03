f6db565144ea1a9406860fa77865d18f12fe3b1e
Gabriel Rodríguez <gabrielrodriguezj@hotmail.com>
2024-04-02 19:56:52 -0600

    Update StmtBlock.cpp

--- src/Inter/Statement/StmtBlock.cpp
+++ src/Inter/Statement/StmtBlock.cpp
@@ -1,7 +1,8 @@
 #include "StmtBlock.h"
+#include "../../Exceptions/NotImplementedYetException.h"
 
 StmtBlock::StmtBlock(const std::list<Statement *> &statements) : statements(statements) {}
 
 void StmtBlock::execute(Environment* environment) {
-
+    throw NotImplementedYetException("Not implemented yet");
 }
